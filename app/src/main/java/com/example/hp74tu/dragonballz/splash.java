4351154d0a402d5cc6f7f9168b3bdd0a726d36ba
AbhiTaker <Abhi.taker20@gmail.com>
2017-05-14 21:05:11 +0530

    splash and main page designed

--- app/src/main/java/com/example/hp74tu/dragonballz/splash.java
+++ app/src/main/java/com/example/hp74tu/dragonballz/splash.java
@@ -1,5 +1,6 @@
 package com.example.hp74tu.dragonballz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,24 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        sp first= new sp();
+
+        first.start();
+    }
+
+    public class sp extends Thread{
+
+        public  void run()
+        {
+            try{
+                sleep(3000);
+            }catch (Exception e){}
+
+            Intent info = new Intent(splash.this , home.class);
+            startActivity(info);
+            finish();
+
+        }
     }
 }
