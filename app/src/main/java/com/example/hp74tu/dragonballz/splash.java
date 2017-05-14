24f02cf40145506f708fd77d7a225895631bc255
AbhiTaker <Abhi.taker20@gmail.com>
2017-05-14 20:57:56 +0530

    resources added

---
+++ app/src/main/java/com/example/hp74tu/dragonballz/splash.java
@@ -0,0 +1,13 @@
+package com.example.hp74tu.dragonballz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
