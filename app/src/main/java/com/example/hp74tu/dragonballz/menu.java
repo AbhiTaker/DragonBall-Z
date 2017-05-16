10a600989fd5ff9e3fb0f806530ff63756f2685f
AbhiTaker <Abhi.taker20@gmail.com>
2017-05-17 00:56:23 +0530

    listactivity linked with other activities

--- app/src/main/java/com/example/hp74tu/dragonballz/menu.java
+++ app/src/main/java/com/example/hp74tu/dragonballz/menu.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import static android.R.attr.id;
 
 public class menu extends ListActivity {
 
-    String classes[]={"home","example1","example2"};
+    String classes[]={"home","GOKU","GOHAN","VEGETA","SUPERSAIYEN","WALLPAPERS"};
+    TextView content;
 
 
 
@@ -20,27 +22,36 @@ public class menu extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
-       // setListAdapter(new ArrayAdapter<String>(menu.this,android.R.layout.simple_list_item_1,classes));
+        content = (TextView)findViewById(R.id.output);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,classes);
 
 
-        // Assign adapter to List
+        // Assign adapter to classes
         setListAdapter(adapter);
     }
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        String act = classes[position];
+        // ListView Clicked item index
+        //int itemPosition     = position;
+
+        // ListView Clicked item value
+        //String  itemValue    = (String) l.getItemAtPosition(position);
+        String itemValue = classes[position];
+
+       // content.setText("Click : \n  Position :"+itemPosition+"  \n  ListItem : " +itemValue);
+
         try{
-            Class ourclass = Class.forName("com.example.hp74tu." + act);
-            Intent open = new Intent(menu.this, ourclass);
+           Class ourclass = Class.forName("com.example.hp74tu.dragonballz." + itemValue);
+            Intent open = new Intent(this, ourclass);
+            startActivity(open);
 
         }catch (ClassNotFoundException e)
         {
             e.printStackTrace();
         }
+
     }
 }
