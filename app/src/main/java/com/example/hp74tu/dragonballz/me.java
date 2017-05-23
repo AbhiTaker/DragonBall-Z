c41cad582b71d35649fe526087e130cba7d3234b
AbhiTaker <Abhi.taker20@gmail.com>
2017-05-24 00:44:56 +0530

    me activity debugged

--- app/src/main/java/com/example/hp74tu/dragonballz/me.java
+++ app/src/main/java/com/example/hp74tu/dragonballz/me.java
@@ -2,6 +2,7 @@ package com.example.hp74tu.dragonballz;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
 
@@ -29,6 +31,8 @@ public class me extends AppCompatActivity implements View.OnClickListener{
         setContentView(R.layout.activity_me);
 
         init();
+        InputStream is = getResources().openRawResource(R.drawable.all);
+        bmp = BitmapFactory.decodeStream(is);
     }
 
     private void init() {
